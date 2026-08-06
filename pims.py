bf7f69f14d8ea8b0e592bdc88fe91e20aa7cb56e
Abhays3 <st23179@howick.school.nz>
2026-08-06 13:34:53 +1200

    Add files via upload
    This is the beginning of my program. moreover, in this file I have made the list of names that will be used throughout the whole program, and I have made the offence_records list

---
+++ pims.py
@@ -0,0 +1,14 @@
+# Patrol Car Infringement Management System (PIMS)
+
+
+WANTED_LIST = [
+    "Abhay Singh",
+    "Spider Man",
+    "Super Man",
+    "Tony Stark",
+    "Shawn jonathan"
+    
+]
+
+#list collection to store all recorded offence dictionaries
+offence_records = []
