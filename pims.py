112976b64e6783ef905a598cc362243200be197f
Abhays3 <st23179@howick.school.nz>
2026-08-07 12:38:24 +1200

    Add files via upload
    Added the def display_menu() function, and added all the options the officers will be able to use

--- pims.py
+++ pims.py
@@ -6,9 +6,23 @@ WANTED_LIST = [
     "Spider Man",
     "Super Man",
     "Tony Stark",
-    "Shawn jonathan"
+    "Shawn Jonathan"
     
 ]
 
 #list collection to store all recorded offence dictionaries
 offence_records = []
+
+def display_menu():
+    """Displays the main menu options for the officer."""
+    print("\n" + "=" * 30)
+    print("     Police Patrol System    ")
+    print("=" * 30)
+    print("1. Record a speeding offence")
+    print("2. View all recorded offences")
+    print("3. Search offence records")
+    print("4. Display patrol summary")
+    print("5. Exist program")
+    print("=" * 30)
+    
+    
